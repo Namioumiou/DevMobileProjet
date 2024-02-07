6cc92649393d6c92c61fe4cc692e3f1e8bd71366
Sarouman54 <sarahbarrabe@gmail.com>
2024-02-07 23:53:48 +0100

    Add Back Button Functionality

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.todolist;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Souhaitez vous fermer l'application ?")
+                        .setTitle("Vous nous quittez déjà...")
+                        .setPositiveButton("Fermer", (dialog, which) -> {
+                            finish();
+                            dialog.dismiss();
+                        })
+                        .setNegativeButton("Annuler", (dialog, which) -> dialog.dismiss())
+                        .show();
+            }
+        });
     }
 }
