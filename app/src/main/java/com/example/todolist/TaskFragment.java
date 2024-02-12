fb615e08705fd0bb35cc4c3f05327d8e2a5cd266
Sarouman54 <sarahbarrabe@gmail.com>
2024-02-12 12:15:15 +0100

    add fragments

---
+++ app/src/main/java/com/example/todolist/TaskFragment.java
@@ -0,0 +1,28 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TaskFragment extends Fragment {
+    public static TaskFragment newInstance() {
+        TaskFragment task = new TaskFragment();
+        return task;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task, container, false);
+    }
+}
